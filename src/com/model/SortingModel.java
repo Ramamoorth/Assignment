90ad042a9af4806bb75a2ab3a8783e2b0854ad7d
Ramamoorth <bhagathram700@gmail.com>
2017-06-19 16:09:30 +0530

    Update SortingModel.java

--- src/com/model/SortingModel.java
+++ src/com/model/SortingModel.java
@@ -1,7 +1,13 @@
 package com.model;
 
+/**
+ * 
+ * @author 582571
+ * Create SortingModel class to create getter and setter methods for sorting object
+ */
 public class SortingModel {
-	 private String sortingList;
+	private String sortingList;
+	private String originalList;
     private long timing;
     private long positionChangeCount;
     
@@ -11,6 +17,14 @@ public class SortingModel {
 
 public void setSortingList(String sortingList) {
         this.sortingList = sortingList;
+}
+
+public String getOriginalList() {
+    return originalList;
+}
+
+public void setOriginalList(String originalList) {
+    this.originalList = originalList;
 }
 
 public long getTiming() {
