f3c15879920f64405e430cf8ff88de123817d397
Ramamoorth <bhagathram700@gmail.com>
2017-06-19 16:08:39 +0530

    Update RandomSortingMethods.java

--- src/Utill/RandomSortingMethods.java
+++ src/Utill/RandomSortingMethods.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,143 +11,83 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.Writer;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.Properties;
 
 import com.google.gson.Gson;
 import com.model.SortingModel;
 
+/** 
+ * @author 582571
+ *Create the RandomSortingMethods which contains the available sorting methods
+ */
 public class RandomSortingMethods {
-	public static int getPositionChanges(int[] arr)
+	
+	static int CeilIndex(int A[], int l, int r, int key)
     {
-        int n = arr.length;
- 
-        // Create two arrays and use as pairs where first
-        // array is element and second array
-        // is position of first element
-        ArrayList <Pair <Integer, Integer> > arrpos =
-                  new ArrayList <Pair <Integer, Integer> > ();
-        for (int i = 0; i < n; i++)
-             arrpos.add(new Pair <Integer, Integer> (arr[i], i));
- 
-        // Sort the array by array element values to
-        // get right position of every element as the
-        // elements of second array.
-        arrpos.sort(new Comparator<Pair<Integer, Integer>>()
+        while (r - l > 1)
         {
-            @Override
-            public int compare(Pair<Integer, Integer> o1,
-                               Pair<Integer, Integer> o2)
-            {
-                if (o1.getValue() > o2.getValue())
-                    return -1;
- 
-                // We can change this to make it then look at the
-                // words alphabetical order
-                else if (o1.getValue().equals(o2.getValue()))
-                    return 0;
- 
-                else
-                    return 1;
-            }
-        });
- 
-        // To keep track of visited elements. Initialize
-        // all elements as not visited or false.
-        Boolean[] vis = new Boolean[n];
-        Arrays.fill(vis, false);
- 
-        // Initialize result
-        int ans = 0;
- 
-        // Traverse array elements
-        for (int i = 0; i < n; i++)
-        {
-            // already swapped and corrected or
-            // already present at correct pos
-            if (vis[i] || arrpos.get(i).getValue() == i)
-                continue;
- 
-            // find out the number of  node in
-            // this cycle and add in ans
-            int cycle_size = 0;
-            int j = i;
-            while (!vis[j])
-            {
-                vis[j] = true;
- 
-                // move to next node
-                j = arrpos.get(j).getValue();
-                cycle_size++;
-            }
- 
-            // Update answer by adding current cycle.
-            ans += (cycle_size - 1);
+            int m = l + (r - l)/2;
+            if (A[m]>=key)
+                r = m;
+            else
+                l = m;
         }
  
-        // Return result
-        return ans;
+        return r;
     }
-    
 	
+	//This method is used to get the no of position changes completed for the particular sorting	
+	 public int getNoOfPositionChanges(int A[], int size)
+	    {
+	        // Add boundary case, when array size is one
+	 
+	        int[] tailTable   = new int[size];
+	        int len; // always points empty slot
+	 
+	        tailTable[0] = A[0];
+	        len = 1;
+	        for (int i = 1; i < size; i++)
+	        {
+	            if (A[i] < tailTable[0])
+	                // new smallest value
+	                tailTable[0] = A[i];
+	 
+	            else if (A[i] > tailTable[len-1])
+	                // A[i] wants to extend largest subsequence
+	                tailTable[len++] = A[i];
+	 
+	            else
+	                // A[i] wants to be current end candidate of an existing
+	                // subsequence. It will replace ceil value in tailTable
+	                tailTable[CeilIndex(tailTable, -1, len-1, A[i])] = A[i];
+	        }
+	 
+	        return len;
+	    }
+	
+	 //This method is used to save the sortingdata to json file	 
 	public static String savesortingData(String sortingModel){   
 		Writer output;
-        try(PrintWriter output1 = new PrintWriter(new FileWriter("C:\\ec\\testout.txt",true))) 
+        try(PrintWriter output1 = new PrintWriter(new FileWriter("C:\\ec\\testout.json",true))) 
         {
             output1.printf("%s\r\n", sortingModel);
         } 
         catch (Exception e) {}
-         System.out.println("Success...");
 		return null;    
-    }    
+    }
 	
+	 // This method is used to display the history of sorted values
 	public ArrayList displaySortingData() throws Exception{    
-        FileReader fr=new FileReader("C:\\ec\\testout.txt");
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\ec\\testout.txt"), "Cp1252"));         
-	      SortingModel sortingModel = new SortingModel();
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\ec\\testout.json"), "Cp1252")); 	      
 	      ArrayList historyArray = new ArrayList();
+	      Gson gson = new Gson();
         String line;    
         while ((line = br.readLine()) != null) {
-        	sortingModel.setSortingList(line);
-        	historyArray.add(line);
+        	SortingModel sortingModel = gson.fromJson(line, SortingModel.class);
+        	historyArray.add(sortingModel);
+        }         
         
-        }
-        System.out.print(historyArray); 
-        fr.close();
 		return historyArray;    
   }    
-	public static String setSortingProp(StringBuffer sortedValues,long timeInMillis, int PositionChange) {
-
-	    Properties prop = new Properties();
-	    OutputStream output = null;
-	    File f = new File("C:\\ec\\config.properties");
-	    try {
-	    	if (f.exists()) {
-	        FileInputStream input = new FileInputStream("C:\\ec\\config.properties");    
-	        prop.load(input);
-	        input.close();
-	    	}
-	        output = new FileOutputStream("C:\\ec\\config.properties");
-	        prop.setProperty("app.sorting", sortedValues.toString());
-	        prop.setProperty("app.timing", String.valueOf(timeInMillis));
-	        prop.setProperty("app.position", String.valueOf(PositionChange));
-	        prop.store(output, null);
-
-
-	    } catch (IOException io) {
-	        io.printStackTrace();
-	    } finally {
-	        if (output != null) {
-	            try {
-	                output.close();
-	            } catch (IOException e) {
-	                e.printStackTrace();
-	            }
-	        }
-
-	    }
-		return null;
-	}
-
-
+	
 }
