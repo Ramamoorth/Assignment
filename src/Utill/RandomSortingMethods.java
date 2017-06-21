18a0cbe70eea79a31d2f3073e80b757b6caf9b7d
Ramamoorth <bhagathram700@gmail.com>
2017-06-21 14:34:53 +0530

    Update RandomSortingMethods.java

--- src/Utill/RandomSortingMethods.java
+++ src/Utill/RandomSortingMethods.java
@@ -11,6 +11,9 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.Writer;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import com.google.gson.Gson;
@@ -22,53 +25,11 @@ import com.model.SortingModel;
  */
 public class RandomSortingMethods {
 	
-	static int CeilIndex(int A[], int l, int r, int key)
-    {
-        while (r - l > 1)
-        {
-            int m = l + (r - l)/2;
-            if (A[m]>=key)
-                r = m;
-            else
-                l = m;
-        }
- 
-        return r;
-    }
-	
-	//This method is used to get the no of position changes completed for the particular sorting	
-	 public int getNoOfPositionChanges(int A[], int size)
-	    {
-	        // Add boundary case, when array size is one
-	 
-	        int[] tailTable   = new int[size];
-	        int len; // always points empty slot
-	 
-	        tailTable[0] = A[0];
-	        len = 1;
-	        for (int i = 1; i < size; i++)
-	        {
-	            if (A[i] < tailTable[0])
-	                // new smallest value
-	                tailTable[0] = A[i];
-	 
-	            else if (A[i] > tailTable[len-1])
-	                // A[i] wants to extend largest subsequence
-	                tailTable[len++] = A[i];
-	 
-	            else
-	                // A[i] wants to be current end candidate of an existing
-	                // subsequence. It will replace ceil value in tailTable
-	                tailTable[CeilIndex(tailTable, -1, len-1, A[i])] = A[i];
-	        }
-	 
-	        return len;
-	    }
 	
 	 //This method is used to save the sortingdata to json file	 
 	public static String savesortingData(String sortingModel){   
 		Writer output;
-        try(PrintWriter output1 = new PrintWriter(new FileWriter("C:\\RandomSorting\\testout.json",true))) 
+        try(PrintWriter output1 = new PrintWriter(new FileWriter("C:\\ec\\testout.json",true))) 
         {
             output1.printf("%s\r\n", sortingModel);
         } 
@@ -78,7 +39,7 @@ public class RandomSortingMethods {
 	
 	 // This method is used to display the history of sorted values
 	public ArrayList displaySortingData() throws Exception{    
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\RandomSorting\\testout.json"), "Cp1252")); 	      
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\ec\\testout.json"), "Cp1252")); 	      
 	      ArrayList historyArray = new ArrayList();
 	      Gson gson = new Gson();
         String line;    
@@ -88,6 +49,33 @@ public class RandomSortingMethods {
         }         
         
 		return historyArray;    
-  }    
+  } 
+	
+	//This method is used to get the no of position changes completed for the particular sorting	
+	public int getNoOfPositionChanges(int[] ar) {
+	    int n = ar.length;
+	    Map<Integer, Integer> m = new HashMap<>();
+	    for (int i = 0; i < n; i++) {
+	        m.put(ar[i], i);
+	    }
+	    Arrays.sort(ar);
+	    for (int i = 0; i < n; i++) {
+	        ar[i] = m.get(ar[i]);
+	    }
+	    m = null;
+	    int swaps = 0;
+	    for (int i = 0; i < n; i++) {
+	        int val = ar[i];
+	        if (val < 0) continue;
+	        while (val != i) {
+	            int new_val = ar[val];
+	            ar[val] = -1;
+	            val = new_val;
+	            swaps++;
+	        }
+	        ar[i] = -1;
+	    }
+	    return swaps;
+	}
 	
 }
